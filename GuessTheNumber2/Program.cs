44c45af240b084c16be75ed5853e28426321641f
Cel Mic Bogdan <cmbogdan11@gmail.com>
2018-12-06 03:11:54 +0200

    guess the number app

--- GuessTheNumber2/Program.cs
+++ GuessTheNumber2/Program.cs
@@ -10,6 +10,23 @@ namespace GuessTheNumber2
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int numberToGuess = random.Next(100);
+            int userGuess = 0;
+            while (userGuess != numberToGuess)
+            {
+                Console.Write("Enter your guess: ");
+                int.TryParse(Console.ReadLine(), out userGuess);
+                if (userGuess > numberToGuess)
+                { Console.WriteLine("{0} is too high!", userGuess);
+                }
+                else if (userGuess < numberToGuess)
+                { Console.WriteLine("{0} is too low!", userGuess);
+                }
+                else
+                { Console.WriteLine("{0} is right! Congratulations.", userGuess);
+                }
+            }
         }
     }
 }
